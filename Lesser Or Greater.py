1fd4e083e74ff985c6ef0663cc2271fc42c6c748
SanidhyaSingla <95517974+SanidhyaSingla@users.noreply.github.com>
2022-11-13 14:27:27 +0530

    Add files via upload

---
+++ Lesser Or Greater.py
@@ -0,0 +1,9 @@
+# Lesser or greater
+
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+
+if a > b:
+    print(a, "is greater than", b)
+else:
+    print(b, "is greater than", a)
